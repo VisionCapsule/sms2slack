3757c43d9abf0581f18a20a2bdab174779eaf1ff
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2020-06-28 17:27:18 +0000

    nest PhET-specific metadata under "phet" in package.json, phetsims/chipper#266

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
 
 const SHA256 = require('crypto-js/SHA256');
 class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
