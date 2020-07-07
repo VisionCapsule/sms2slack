cf1242bcd31d833dbc4403ccbc280c9b87677c5c
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2020-07-07 09:45:06 +0000

    Updated dependancies

--- block.js
+++ block.js
@@ -8,3 +8,6 @@ class Block {
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
