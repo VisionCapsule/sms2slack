8a6cfbe28c1b8ca98ce1440328e0e37d898f022c
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2021-03-23 13:30:57 +0000

    doing

--- blockchain.js
+++ blockchain.js
@@ -2,4 +2,4 @@ const Block = require('./block.js');
 
 class Blockchain {
     constructor(){
-        this.chain=[this.createGenesisBlock()];//creates a new chain with a block set by out
+        this.chain=[this.createGenesisBlock()];//creates a new chain with a block set by out c
