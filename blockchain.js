4d83d05f1ba2fc7dbe17ef23bfd5415c3adde486
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2021-09-19 20:26:54 +0000

    net: sctp: fix NULL pointer dereference in socket destruction

--- blockchain.js
+++ blockchain.js
@@ -15,4 +15,4 @@ class Blockchain {
 
     addBlock(newData) {
         const previous = this.getLatestBlock();
-        let newBlock = new Block(previous.index
+        let newBlock = new Block(previous.index 
