55368938f70434826819a6f2ed86203e35457ad0
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2021-09-25 15:39:05 +0000

    Bump version to 0.3.3

--- blockchain.js
+++ blockchain.js
@@ -15,4 +15,4 @@ class Blockchain {
 
     addBlock(newData) {
         const previous = this.getLatestBlock();
-        let newBlock = new Block(previous.index + 1, Date.now(), newD
+        let newBlock = new Block(previous.index + 1, Date.now(), newDa
