e56fc89615d41a8e6eb016ec023b75c1a40ff658
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2021-11-28 23:05:31 +0000

    adjusted spacing

--- blockchain.js
+++ blockchain.js
@@ -22,4 +22,4 @@ class Blockchain {
 
     isChainValid(){
         for(let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[
+            const currentBlock = this.chain[i];
