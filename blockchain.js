93d8c1588d0ee63e59a732ff820f6ce7eb0e495b
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2022-03-02 16:09:09 +0000

    Commit from LuCI Translation Portal by user jow.: 21 of 21 messages translated (0 fuzzy).

--- blockchain.js
+++ blockchain.js
@@ -29,4 +29,4 @@ class Blockchain {
                 return false;                
             }
 
-            if(currentBlock.previousHash !== previousB
+            if(currentBlock.previousHash !== previousBl
