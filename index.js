85cc9202a141f4af758ca5517721740e64b591d7
VisionCapsule <106441972+VisionCapsule@users.noreply.github.com>
2022-06-06 20:46:29 +0000

    updated regex

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const Blockchain = require('./blockchain.js');
+const Block = require('./block.js');
+const newBlockchain = new Blockchain();
